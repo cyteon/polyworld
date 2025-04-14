6f44d4aaa00b958952c07c5237e00f4ad0b7fffc
cyteon <cyteon@hackclub.app>
2025-04-14 12:01:56 +0200

    some more stuff

--- scripts/globals/Steamworks.gd
+++ scripts/globals/Steamworks.gd
@@ -2,12 +2,16 @@ extends Node
 
 var steam_started: bool = false
 var app_owner: int = 0
+var steam_id: int = 0
+var steam_username: String = ""
 var is_owned: bool = false
 
 var steam_api
 
 func _ready() -> void:
 	if Engine.has_singleton("Steam"):
+		print("Steam Singleton found")
+		
 		steam_api = Engine.get_singleton("Steam")
 		var initialize_response: Dictionary = steam_api.steamInitEx()
 		print("Did Steam initialize?: %s " % initialize_response)
@@ -16,6 +20,14 @@ func _ready() -> void:
 			steam_started = true
 			is_owned = steam_api.isSubscribed()
 			app_owner = steam_api.getAppOwner()
+			steam_id = steam_api.getSteamID()
+			steam_username = steam_api.getPersonaName()
+			
+			print("Game is %s, running as %s (%s)" % [
+				("owned by %s" % app_owner) if is_owned else "not owned",
+				steam_username,
+				steam_id
+			])
 
 func _process(delta: float) -> void:
 	if steam_started:
