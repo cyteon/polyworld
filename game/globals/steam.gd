c1e6decc19fe8dedcd2d775d4ea9d295aaa326dc
cyteon <cyteon@hackclub.app>
2025-09-22 20:07:12 +0200

    blep

--- game/globals/steam.gd
+++ game/globals/steam.gd
@@ -4,5 +4,5 @@ func _ready() -> void:
 	var initialize_response: Dictionary = Steam.steamInitEx()
 	print("SteamWorks | %s" % initialize_response)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	Steam.run_callbacks()
